a5bf69b12d1c13990af9a78f1bc8898e3e4b65bf
Angel Argumedo <angel.david.argumedo.guerra@gmail.com>
2025-11-22 17:37:01 -0500

    add: tablas de verdad

--- algorithms/cursocpp/seleccion/operadoresLogicos.cpp
+++ algorithms/cursocpp/seleccion/operadoresLogicos.cpp
@@ -5,6 +5,7 @@ int main() {
 
   bool valores[] = {false, true};
 
+  std::cout << "" << std::endl;
   std::cout << "Tabla de verdad para el operador AND" << std::endl;
   std::cout << "A\tB\tA && B\n";
   std::cout << "-------------------------\n";
@@ -13,6 +14,25 @@ int main() {
     for (bool B : valores)
       std::cout << A << "\t" << B << "\t" << ((A && B) ? "1" : "0")
                 << std::endl;
+
+  std::cout << "" << std::endl;
+  std::cout << "Tabla de verdad para el operador OR" << std::endl;
+  std::cout << "A\tB\tA || B\n";
+  std::cout << "-------------------------\n";
+
+  for (bool A : valores)
+    for (bool B : valores)
+      std::cout << A << "\t" << B << "\t" << ((A || B) ? "1" : "0")
+                << std::endl;
+
+  std::cout << "" << std::endl;
+  std::cout << "Tabla de verdad para el operador XOR" << std::endl;
+  std::cout << "A\tB\tA ^ B\n";
+  std::cout << "-------------------------\n";
+
+  for (bool A : valores)
+    for (bool B : valores)
+      std::cout << A << "\t" << B << "\t" << ((A ^ B) ? "1" : "0") << std::endl;
 
   return 0;
 }
