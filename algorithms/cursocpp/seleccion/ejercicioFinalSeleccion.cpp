9108fa1c13519a3672eceb6422ed6fd31a693191
Angel Argumedo <angel.david.argumedo.guerra@gmail.com>
2025-11-25 19:41:58 -0500

    texto add

--- algorithms/cursocpp/seleccion/ejercicioFinalSeleccion.cpp
+++ algorithms/cursocpp/seleccion/ejercicioFinalSeleccion.cpp
@@ -3,6 +3,14 @@
 
 int main() {
 
-  std::cout << "ejercicio de tic-tac" << std::endl;
+  std::cout << "Este programa te permite jugar Tic tac de una forma basica."
+            << std::endl;
+
+  std::cout << "Como jugar? \n"
+               "se jugaran por turnos para dos jugadores \n"
+               "se puede colocar unicamente \"X\" o \"O\" \n"
+               "Se debe ingresar las coordenadas de la siguiente forma B2X"
+            << std::endl;
+
   return 0;
 }
