29263beec208f3f542628c411315ec1b1b32e078
Angel Argumedo <angel.david.argumedo.guerra@gmail.com>
2025-11-27 23:14:24 -0500

    avance ejercicio final

--- algorithms/cursocpp/seleccion/ejercicioFinalSeleccion.cpp
+++ algorithms/cursocpp/seleccion/ejercicioFinalSeleccion.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <string>
 
 int main() {
 
@@ -12,5 +13,28 @@ int main() {
                "Se debe ingresar las coordenadas de la siguiente forma B2X"
             << std::endl;
 
+  char A1 = '#', A2 = '#', A3 = '#';
+  char B1 = '#', B2 = '#', B3 = '#';
+  char C1 = '#', C2 = '#', C3 = '#';
+
+  std::string eleccionJ1;
+  std::string eleccionJ2;
+
+  std::cout << A1 << A2 << A3 << std::endl;
+  std::cout << B1 << B2 << B3 << std::endl;
+  std::cout << C1 << C2 << C3 << std::endl;
+
+  std::cout << "jugador 1: ";
+  std::cin >> eleccionJ1;
+
+  if (eleccionJ1 != "") {
+
+    char fila = eleccionJ1[0];
+    char columna = eleccionJ1[1];
+    char letra = eleccionJ1[2];
+
+  } else {
+    std::cout << "no seleccionaste nada" << std::endl;
+  }
   return 0;
 }
