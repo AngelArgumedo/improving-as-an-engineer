eb1a0e581b27fa28009dfcd31046a2f4e084c494
Angel Argumedo <angel.david.argumedo.guerra@gmail.com>
2025-12-17 22:03:26 -0500

    add: for anidado

--- algorithms/cursocpp/RepeticionCondicional/anidacionFor.cpp
+++ algorithms/cursocpp/RepeticionCondicional/anidacionFor.cpp
@@ -5,5 +5,17 @@ using namespace std;
 int main() {
   cout << "Anidacion del ciclo for" << endl;
 
+  int lado;
+
+  cout << "Lado ";
+  cin >> lado;
+
+  for (int i = 1; i <= lado; i++) {
+    for (int j = 1; j <= lado; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
   return 0;
 }
